73b8516ab2d59b903028bee97cd5f406a28d8eef
Ionică Bizău <bizauionica@gmail.com>
2016-11-14 21:17:09 +0200

    Example when using different sizes

--- example/index.js
+++ example/index.js
@@ -2,10 +2,17 @@
 
 const imgSSIM = require("../lib");
 
+//imgSSIM(
+//    "https://octodex.github.com/images/original.png"
+//  , "https://octodex.github.com/images/class-act.png"
+//  , (err, similarity) => {
+//    console.log(err || similarity);
+//});
+
 imgSSIM(
     "https://octodex.github.com/images/original.png"
-  //, "https://octodex.github.com/images/privateinvestocat.jpg"
-  , "https://octodex.github.com/images/class-act.png"
+  , "https://ionicabizau.net/images/logo.png"
+  , { enforceSameSize: false }
   , (err, similarity) => {
     console.log(err || similarity);
 });
