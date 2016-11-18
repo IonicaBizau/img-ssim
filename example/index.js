a37a96ff0549770ef17f301dabd7a8277bd7d0fa
Ionică Bizău <bizauionica@gmail.com>
2016-11-18 15:04:42 +0200

    Docs

--- example/index.js
+++ example/index.js
@@ -2,17 +2,19 @@
 
 const imgSSIM = require("../lib");
 
-//imgSSIM(
-//    "https://octodex.github.com/images/original.png"
-//  , "https://octodex.github.com/images/class-act.png"
-//  , (err, similarity) => {
-//    console.log(err || similarity);
-//});
+imgSSIM(
+    "https://octodex.github.com/images/original.png"
+  , "https://octodex.github.com/images/class-act.png"
+  , (err, similarity) => {
+    console.log(err || similarity);
+    // => 0.7683075604309328
+});
 
 imgSSIM(
     "https://octodex.github.com/images/original.png"
   , "https://ionicabizau.net/images/logo.png"
-  , { enforceSameSize: false }
+  , { enforceSameSize: false, resize: true }
   , (err, similarity) => {
     console.log(err || similarity);
+    // => 0.2631629323319616
 });
